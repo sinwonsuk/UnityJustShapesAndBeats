26fa08450d38587dca8b9018a07e186f2ea46f16
kang-dani <daeun4001@gmail.com>
2025-03-21 00:08:28 +0900

    다은 - OneStagePase2 Update

--- Assets/Script/신원석/OneStagePase/OneStagePase2.cs
+++ Assets/Script/신원석/OneStagePase/OneStagePase2.cs
@@ -1,19 +1,40 @@
+using NUnit.Framework.Interfaces;
 using UnityEngine;
 
 public class OneStagePase2 : State<OneStage>
 {
-    public override void Enter(OneStage entity)
-    {
-        
-    }
+	public override void Enter(OneStage entity)
+	{
 
-    public override void Execute(OneStage entity)
-    {
-     
-    }
+	}
 
-    public override void Exit(OneStage entity)
-    {
-     
-    }
+	public override void Execute(OneStage entity)
+	{
+		time += Time.deltaTime;
+		entity.StartPattern(entity.Getpattern(EPattern.kickmove));
+		if (time >= 7f && time < 7.1f)
+		{
+			entity.StartPattern(entity.Getpattern(EPattern.UppercutPattern));
+			entity.StopPattern(entity.Getpattern(EPattern.kickmove));
+		}
+		else if (time >= 19f && time < 19.1f)
+		{
+			entity.StartPattern(entity.Getpattern(EPattern.HadoukenPattern));
+		}
+		else if (time >= 20f && time < 20.1f)
+		{
+			entity.StopPattern(entity.Getpattern(EPattern.UppercutPattern));
+		}
+		else if (time >= 20.3f)
+		{
+			entity.ChangeState(StagePase.Pase3);
+		}
+	}
+
+	public override void Exit(OneStage entity)
+	{
+		time = 0f;
+	}
+
+	private float time = 0f;
 }
