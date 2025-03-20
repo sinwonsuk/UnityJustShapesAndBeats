b91aa74ef379823e8a1b6e54ff69935718615704
mahara <yeji301477@gmail.com>
2025-03-20 22:07:36 +0900

    sdfsd
    ...

--- Assets/Script/신원석/OneStagePase3.cs
+++ Assets/Script/신원석/OneStagePase3.cs
@@ -1,19 +1,30 @@
 using UnityEngine;
+using System.Collections;
 
-public class OneStagePase3 : State<OneStage>
+public class MyScript3 : State<OneStage>
 {
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public override void Enter(OneStage entity)
     {
-        
+        entity.StartPattern(entity.Getpattern(EPattern.kickmove));
+        entity.StartPattern(entity.Getpattern(EPattern.UppercutPattern));
+     
+
+        entity.StopTimePattern(entity.Getpattern(EPattern.kickmove),14);
+        entity.StopTimePattern(entity.Getpattern(EPattern.UppercutPattern),14);
+     
     }
 
     public override void Execute(OneStage entity)
     {
-     
+
+        
     }
 
     public override void Exit(OneStage entity)
     {
-     
+
     }
+
 }
