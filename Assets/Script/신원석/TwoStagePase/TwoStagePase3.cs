1cb5965c01bbe01269534b28297df6fb67f484c7
bogeonK <130254026+bogeonK@users.noreply.github.com>
2025-03-25 19:20:49 +0900

    2스테이지 마지막구간

--- Assets/Script/신원석/TwoStagePase/TwoStagePase3.cs
+++ Assets/Script/신원석/TwoStagePase/TwoStagePase3.cs
@@ -5,7 +5,13 @@ public class TwoStagePase3 : State<TwoStage>
 
     public override void Enter(TwoStage entity)
     {
-      
+        GameObject boxPattern = entity.Getpattern(TwoEPattern.BoxFadePatternManager);
+        BoxFadePatternSpawnManager manager = boxPattern.GetComponent<BoxFadePatternSpawnManager>();
+        manager.SetPattern((int)T.Three);
+        entity.StartTimePattern((boxPattern), 9f);
+
+        entity.StartTimePattern(entity.Getpattern(TwoEPattern.SnowBall_slow), 15.5f);
+        entity.StopTimePattern(entity.Getpattern(TwoEPattern.SnowBall_slow), 30f);
     }
 
     public override void Execute(TwoStage entity)
