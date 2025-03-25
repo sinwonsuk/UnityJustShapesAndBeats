50f94851a596a7974ae2f2c3a23a01e714dddbc3
mahara <yeji301477@gmail.com>
2025-03-26 01:20:00 +0900

    오전 1:19
    ...

--- Assets/Script/신원석/TwoStagePase/TwoStagePase1.cs
+++ Assets/Script/신원석/TwoStagePase/TwoStagePase1.cs
@@ -1,11 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class TwoStagePase1 : State<TwoStage>
 {
-
     public override void Enter(TwoStage entity)
     {
-      
+        entity.StartPattern(entity.Getpattern(TwoEPattern.SnowBall_first));
+        entity.StopTimePattern(entity.Getpattern(TwoEPattern.SnowBall_first), 22);
+
+        entity.StartTimePattern(entity.Getpattern(TwoEPattern.triangle_spawner), 8);
+        entity.StartTimePattern(entity.Getpattern(TwoEPattern.triangle_spawner_2), 8);
+        entity.StartTimePattern(entity.Getpattern(TwoEPattern.miniball), 8);
+        entity.StartTimePattern(entity.Getpattern(TwoEPattern.miniball_2), 8);
+
+        entity.StartTimePattern(entity.Getpattern(TwoEPattern.BoxFadePatternManager), 15);
+        entity.StopTimePattern(entity.Getpattern(TwoEPattern.BoxFadePatternManager), 27);
+
+        entity.StartTimePattern(entity.Getpattern(TwoEPattern.UglyCircle), 26);
+        time = 0;
     }
 
     public override void Execute(TwoStage entity)
@@ -15,6 +27,9 @@ public class TwoStagePase1 : State<TwoStage>
 
     public override void Exit(TwoStage entity)
     {
-
+        
     }
+
+
+    float time = 0;
 }
