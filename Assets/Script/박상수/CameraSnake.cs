318fc1cc96b0c7ce8491b4d503cf767db29602c1
tkdtn404 <92982995+tkdtn404@users.noreply.github.com>
2025-03-26 11:48:02 +0900

    원 패턴(수정) 및 카메라

--- Assets/Script/박상수/CameraSnake.cs
+++ Assets/Script/박상수/CameraSnake.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class CameraSnake : MonoBehaviour
 {
-    public CinemachineCamera cineCamera; // "Cinemachine Camera" 오브젝트를 드래그
-
+    public CinemachineCamera cineCamera;
     private CinemachineBasicMultiChannelPerlin noise;
 
     void Awake()
     {
-        // 하위(혹은 자신)에 붙은 Noise 컴포넌트를 찾습니다.
         noise = cineCamera.GetComponentInChildren<CinemachineBasicMultiChannelPerlin>();
     }
 
+    // 반드시 public으로 선언
     public void ShakeCamera(float amplitude, float frequency, float duration)
     {
         StartCoroutine(Shake(amplitude, frequency, duration));
     }
 
-    private System.Collections.IEnumerator Shake(float amplitude, float frequency, float duration)
+    private IEnumerator Shake(float amplitude, float frequency, float duration)
     {
         noise.AmplitudeGain = amplitude;
         noise.FrequencyGain = frequency;
