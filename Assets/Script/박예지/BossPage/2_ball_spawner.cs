fc279c5a3dceaff0cf429cdcc1a0aa1ac756e273
mahara <yeji301477@gmail.com>
2025-03-27 14:37:23 +0900

    파일정리/얼굴 도깨비불총정리

--- Assets/Script/박예지/BossPage/2_ball_spawner.cs
+++ Assets/Script/박예지/BossPage/2_ball_spawner.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 
 public class ball_spawner2 : MonoBehaviour
+
 {
+    public GameObject Bbullet;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        for (int i = 0; i < 12; i++)
+        {
+            GameObject go = Instantiate(Bbullet, transform.position, Quaternion.Euler(0, 0, 30 * i));
+        }
     }
 
     // Update is called once per frame
