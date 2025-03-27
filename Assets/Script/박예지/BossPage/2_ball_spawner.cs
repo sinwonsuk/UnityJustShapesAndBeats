c8f5bba92784bb6acab650664953b49001172972
mahara <yeji301477@gmail.com>
2025-03-27 15:38:44 +0900

    도깨비불 쏘기 패턴

--- Assets/Script/박예지/BossPage/2_ball_spawner.cs
+++ Assets/Script/박예지/BossPage/2_ball_spawner.cs
@@ -1,21 +1,23 @@
 using UnityEngine;
+using System.Collections;
 
 public class ball_spawner2 : MonoBehaviour
-
 {
     public GameObject Bbullet;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
+        StartCoroutine(StartDelay()); // 2초 대기 후 실행
         for (int i = 0; i < 12; i++)
         {
             GameObject go = Instantiate(Bbullet, transform.position, Quaternion.Euler(0, 0, 30 * i));
         }
+
+        Destroy(gameObject, 18f); // 18초 뒤에 삭제
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator StartDelay()
     {
-        
+        yield return new WaitForSeconds(2f); // 2초 대기
     }
 }
---
+++ Assets/Script/박예지/BossPage/ssoajigi.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+
+public class ssoajigi : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(StartDelay()); // 2초 대기 후 이동 시작
+    }
+
+    IEnumerator StartDelay()
+    {
+        yield return new WaitForSeconds(2f); // 2초 대기
+
+        while (true) // 2초 후에 무한 루프로 이동 시작
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * 5);
+            yield return null; // 다음 프레임까지 대기
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
