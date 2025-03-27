76fe47ab3c96f925ae047bf2ffbf37a3c33efbe9
mahara <yeji301477@gmail.com>
2025-03-27 10:12:28 +0900

    원8등분 수정(속도만 손볼게요)

--- Assets/Script/박예지/ugly/Invisible.cs
+++ Assets/Script/박예지/ugly/Invisible.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class Invisible : MonoBehaviour
 {
+    
+    private void Start()
+    {
+        
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * Time.deltaTime * 5);
+    }
+
+
     void OnBecameInvisible()
     {
         Destroy(gameObject);
