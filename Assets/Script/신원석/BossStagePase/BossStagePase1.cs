8229eab244a9cb3459a30cd765adaba05a5fb866
mahara <yeji301477@gmail.com>
2025-03-28 15:56:49 +0900

    보스 지그재그 다시 만듦

--- Assets/Script/신원석/BossStagePase/BossStagePase1.cs
+++ Assets/Script/신원석/BossStagePase/BossStagePase1.cs
@@ -10,7 +10,9 @@ public class BossStagePase1 : State<BossStage>
         entity.StartPattern(entity.Getpattern(BossEPattern.white_one_0));                       
         entity.StartPattern(entity.Getpattern(BossEPattern.big_white_one_0));
         entity.StartPattern(entity.Getpattern(BossEPattern.idle_BossSlayer));
-     
+        entity.StartTimePattern(entity.Getpattern(BossEPattern.Boss_BossBulletSpawn), 18);
+
+
     }
     float time = 0;
     public override void Execute(BossStage entity)
