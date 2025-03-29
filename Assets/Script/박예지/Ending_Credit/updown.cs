defa5e4ea0f26f3e0518bc6523949356a88c6638
mahara <yeji301477@gmail.com>
2025-03-30 02:09:27 +0900

    글씨 띄우기/스크립트 적용

--- Assets/Script/박예지/Ending_Credit/updown.cs
+++ Assets/Script/박예지/Ending_Credit/updown.cs
@@ -1,18 +1,22 @@
 using UnityEngine;
+using UnityEngine.Scripting;
 
 public class UpDown : MonoBehaviour
 {
-    public Vector2 initialPosition = new Vector2(0, 0); // 초기 위치 (인스펙터에서 설정)
-
+    public float speed = 50f;
     void Start()
     {
-        // 초기 위치 설정
-        transform.position = new Vector3(initialPosition.x, initialPosition.y, transform.position.z);
+        
+        aaa = GetComponent<RectTransform>();
+        
     }
 
     void Update()
     {
-        // 아래로 이동
-        transform.Translate(Vector2.up * Time.deltaTime * 3);
+        // 위로 이동
+        aaa.Translate(Vector3.up * Time.deltaTime * speed);
+        
     }
+    RectTransform aaa;
+    
 }
